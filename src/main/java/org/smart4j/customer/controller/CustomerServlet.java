52f3795d05a293abf05b5833da94a1105c67ccfb
minger <lymlzjtu@163.com>
2018-08-02 22:54:53 +0800

    customer 基础架子完成

--- src/main/java/org/smart4j/customer/controller/CustomerServlet.java
+++ src/main/java/org/smart4j/customer/controller/CustomerServlet.java
@@ -1,5 +1,6 @@
 package org.smart4j.customer.controller;
 
+import org.smart4j.customer.model.Customer;
 import org.smart4j.customer.service.CustomerService;
 
 import javax.servlet.ServletException;
@@ -8,20 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Yuming-Liu
  * 日期： 2018-07-30
  * 时间： 21:51
  */
-@WebServlet("/customer-create")
+@WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
 
     private  CustomerService customerService = CustomerService.getCustomerService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
+
     }
 
     @Override
